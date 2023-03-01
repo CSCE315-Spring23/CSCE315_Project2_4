5f655c0d6e28f4e9625b8fdfb0de22576c2a06e3
Lucas Wilber <s109099@tamu.edu>
2023-02-28 22:42:34 -0600

    clean up and update .csv files and add .sql scripts to create tables and populate them

---
+++ data/makeTables.sql
@@ -0,0 +1,51 @@
+
+CREATE TABLE Inventory (
+    ingredientID int PRIMARY KEY,
+    name text,
+    currAmount float,
+    unit text,
+    minAmount float,
+    cost float
+);
+
+CREATE TABLE ItemIngredients (
+    id int PRIMARY KEY,
+    menuItemID int,
+    ingredientID int,
+    qty int
+);
+
+CREATE TABLE MenuItems (
+    menuItemID int PRIMARY KEY,
+    name text, 
+    menuPrice float,
+    classID int
+);
+
+CREATE TABLE OrderLineItems (
+    lineItemID int PRIMARY KEY,
+    menuItemID int,
+    menuPrice float,
+    orderID int
+);
+
+CREATE TABLE Orders (
+    orderID int PRIMARY KEY,
+    orderTime timestamp, 
+    totalPrice float,
+    employeeID int
+);*/
+
+CREATE TABLE InventoryTransactions (
+    transactionID int PRIMARY KEY,
+    orderID int, 
+    orderTime timestamp,
+    ingredientID int,
+    qty int
+);
+
+CREATE TABLE Employees (
+    employeeID int PRIMARY KEY,
+    name text, 
+    title int
+);
---
+++ data/populate.sql
@@ -0,0 +1,11 @@
+\copy inventory from 'data/Inventory.csv' CSV HEADER
+
+\copy itemingredients from 'data/ItemIngredients.csv' CSV HEADER
+
+\copy menuitems from 'data/MenuItems.csv' CSV HEADER
+
+\copy orderlineitems from 'data/OrderLineItems.csv' CSV HEADER
+
+\copy orders from 'data/Orders.csv' CSV HEADER
+
+\copy employees from 'data/Employees.csv' CSV HEADER

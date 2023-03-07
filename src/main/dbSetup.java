6a4f680562bcc67602957322a0153da268abc5a7
Lucas Wilber <s109099@tamu.edu>
2023-03-07 16:25:37 -0600

    create directories for manager and server views

--- src/main/dbSetup.java
+++
@@ -1,9 +0,0 @@
-/*
-CSCE 315
-Database setup example
-Purpose: Keeping usernames and password private.
-*/
-public final class dbSetup  {
-  public static final String user = "csce315331_team_4_master";
-  public static final String pswd = "@12345";
-}//end class

db80631167d978df1cac2fc6dea67f5521b7c54d
Anh Nguyen <ng.huyanh.gd@gmail.com>
2023-03-07 14:13:05 -0600

    orderLineItem

--- src/main/order.java
+++ src/main/order.java
@@ -1,8 +1,8 @@
 import java.time.*;
 import java.util.*;
 
-public class order{
-	// attributes
+public class order {
+    // attributes
     private int orderID;
     private LocalDateTime orderTime;
     private int totalPrice = 0;
@@ -10,63 +10,73 @@ public class order{
     private List<orderLineItem> orderItems = new ArrayList<orderLineItem>();
 
     // constructor(s)
-    public order(int id, int empID){
+    public order(int id, int empID) {
         orderID = id;
         employeeID = empID;
     }
 
     // getters and setters
-    public int getOrderID(){
+    public int getOrderID() {
         return orderID;
     }
-    public LocalDateTime getOrderTime(){
+
+    public LocalDateTime getOrderTime() {
         return orderTime;
     }
-    public int getTotalPrice(){
+
+    public int getTotalPrice() {
         return totalPrice;
     }
-    public int getEmployeeID(){
+
+    public int getEmployeeID() {
         return employeeID;
     }
-    public List<orderLineItem> getOrderItems(){
+
+    public List<orderLineItem> getOrderItems() {
         return orderItems;
     }
-    public order getFinalizedOrder(){
+
+    public order getFinalizedOrder() {
         orderTime = LocalDateTime.now();
         totalPrice = sumPrices();
         return this;
     }
-    
-    public void setOrderID(int id){
+
+    public void setOrderID(int id) {
         orderID = id;
     }
-    public void getOrderTime(LocalDateTime time){
+
+    public void getOrderTime(LocalDateTime time) {
         orderTime = time;
     }
-    public void getTotalPrice(int price){
+
+    public void getTotalPrice(int price) {
         totalPrice = price;
     }
-    public void getEmployeeID(int id){
+
+    public void getEmployeeID(int id) {
         employeeID = id;
     }
 
     // helper functs
-    public void addOrderItem(orderLineItem item){
+    public void addOrderItem(orderLineItem item) {
         orderItems.add(item);
     }
-    public void deleteOrderItem(orderLineItem item){
+
+    public void deleteOrderItem(orderLineItem item) {
         orderItems.remove(item);
     }
-    public int sumPrices(){
+
+    public int sumPrices() {
         int sum = 0;
-        for(orderLineItem item : orderItems){
-            sum += item.price;
+        for (orderLineItem item : orderItems) {
+            sum += item.getPrice();
         }
         return sum;
     }
 
     // NOTES:
-        // prices will be gathered from sql qurries(and can be intitalized to zero, 
-        // using sql to set the price) whie totalPrice will be calculated from 
-        // locally stored data in the orderlineitem object instances
+    // prices will be gathered from sql qurries(and can be intitalized to zero,
+    // using sql to set the price) whie totalPrice will be calculated from
+    // locally stored data in the orderlineitem object instances
 }

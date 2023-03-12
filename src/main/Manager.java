e3a35979c5fd412aff0ed06d644858acf7a70e95
Sid Prabhakaran <s2siddhu@gmail.com>
2023-03-11 17:37:37 -0800

    Formatted all files to adhere to style guide

--- src/main/Manager.java
+++ src/main/Manager.java
@@ -1,26 +1,25 @@
-//package server;
-	
+// package server;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
-import javafx.fxml.FXMLLoader;
-
+import javafx.stage.Stage;
 
 public class Manager extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			HBox root = (HBox)FXMLLoader.load(getClass().getResource("managerView.fxml"));
-			Scene scene = new Scene(root,600,400);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            HBox root = (HBox) FXMLLoader.load(getClass().getResource("managerView.fxml"));
+            Scene scene = new Scene(root, 600, 400);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/ServerController.java
+++ src/main/ServerController.java
@@ -7,22 +7,22 @@ import javafx.scene.control.*;
 public class ServerController {
     private Vector<Integer> currOrder = new Vector<Integer>();
     private jdbcpostgreSQL db = new jdbcpostgreSQL();
-    private int employeeID = 0; //TODO get session user's (employee) id
+    private int employeeID = 0; // TODO get session user's (employee) id
     @FXML private Label totalPriceField;
 
-    public void initialize(){
-        
-    }
+    public void initialize() {}
 
-    @FXML private void addItemToOrder(ActionEvent event) {
-        Node node = (Node) event.getSource() ;
+    @FXML
+    private void addItemToOrder(ActionEvent event) {
+        Node node = (Node) event.getSource();
         String data = (String) node.getUserData();
         int menuID = Integer.parseInt(data);
         currOrder.add(menuID);
         updateTotal(db.getOrderTotal(currOrder));
     }
 
-    @FXML private void confirmOrder(){
+    @FXML
+    private void confirmOrder() {
         int orderID = db.getNewOrderId();
         db.updateOrdersAndOrderLineItemsTable(currOrder, employeeID, orderID);
         db.updateInventoryTransactionsAndInventoryTable(orderID);
@@ -30,8 +30,8 @@ public class ServerController {
         updateTotal(db.getOrderTotal(currOrder));
     }
 
-    private void updateTotal(float orderTotal){
-        totalPriceField.setText(String.format("%.2f",orderTotal));
+    private void updateTotal(float orderTotal) {
+        totalPriceField.setText(String.format("%.2f", orderTotal));
     }
 }
 
@@ -44,8 +44,8 @@ public class ServerController {
         }
 
         //button functions
-        
-        
+
+
         }
 
     }
